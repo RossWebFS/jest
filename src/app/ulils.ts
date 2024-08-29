e40f2f8a91db94474b9e3d70d8724184df9ba78b
RossWebFS <shimon.vip2006@gmail.com>
2024-08-30 01:08:02 +0300

    Created getStringInfo tests and refactored toUpper and getStringInfo tests

--- src/app/ulils.ts
+++ src/app/ulils.ts
@@ -1,1 +1,17 @@
+export type StringInfo = {
+  lowerCase: string;
+  upperCase: string;
+  characters: string[];
+  length: number;
+  extraInfo?: Object;
+};
+
 export const toUpper = (arg: string) => arg.toUpperCase();
+
+export const getStringInfo = (arg: string): StringInfo => ({
+  lowerCase: arg.toLowerCase(),
+  upperCase: arg.toUpperCase(),
+  characters: Array.from(arg),
+  length: arg.length,
+  extraInfo: {},
+});
